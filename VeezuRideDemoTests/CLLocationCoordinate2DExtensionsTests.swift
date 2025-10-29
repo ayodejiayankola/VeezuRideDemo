31fa3bbd19b8647f7aab12347bdd0799b406a4dd
Ayodeji Ayankola <your_verified_github_email@example.com>
2025-10-29 16:51:31 +0000

    test: add comprehensive unit tests for coordinate utilities and fare calculation

--- VeezuRideDemoTests/CLLocationCoordinate2DExtensionsTests.swift
+++ VeezuRideDemoTests/CLLocationCoordinate2DExtensionsTests.swift
@@ -1,36 +1,129 @@
-//
-//  VeezuRideDemoTests.swift
-//  VeezuRideDemoTests
-//
-//  Created by Ayodeji Ayankola on 29/10/2025.
-//
-
 import XCTest
+import CoreLocation
 @testable import VeezuRideDemo
 
-final class VeezuRideDemoTests: XCTestCase {
+final class CLLocationCoordinate2DExtensionsTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    // MARK: - Distance Tests
+
+    func testDistance_SameCoordinate_ReturnsZero() {
+        
+        let coordinate = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+
+        let distance = coordinate.distance(to: coordinate)
+
+        XCTAssertEqual(distance, 0, accuracy: 1.0)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testDistance_KnownDistance_CalculatesCorrectly() {
+        let start = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+        let end = CLLocationCoordinate2D(latitude: 51.4900, longitude: -3.1791)
+
+        let distance = start.distance(to: end)
+
+        XCTAssertGreaterThan(distance, 900)
+        XCTAssertLessThan(distance, 1100)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    // MARK: - Bearing Tests
+
+    func testBearing_NorthDirection_ReturnsZero() {
+        let start = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+        let north = CLLocationCoordinate2D(latitude: 51.4916, longitude: -3.1791)
+
+        let bearing = start.bearing(to: north)
+
+        XCTAssertLessThan(bearing, 5, "Bearing directly north should be close to 0°")
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testBearing_EastDirection_Returns90() {
+        let start = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+        let east = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1691)
+
+        let bearing = start.bearing(to: east)
+
+        XCTAssertGreaterThan(bearing, 85, "Bearing directly east should be close to 90°")
+        XCTAssertLessThan(bearing, 95)
+    }
+
+    // MARK: - Coordinate Movement Tests
+
+    func testCoordinateAtDistance_MovesCorrectly() {
+    
+        let start = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+        let distance: CLLocationDistance = 1000
+        let bearing: Double = 0
+
+        let result = start.coordinate(at: distance, bearing: bearing)
+
+        XCTAssertGreaterThan(result.latitude, start.latitude, "Moving north should increase latitude")
+        XCTAssertEqual(result.longitude, start.longitude, accuracy: 0.001, "Moving north shouldn't change longitude much")
+    }
+
+    func testCoordinateAtDistance_RoundTrip_ReturnsToOrigin() {
+        
+        let start = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+        let distance: CLLocationDistance = 1000
+
+        let north = start.coordinate(at: distance, bearing: 0)
+        let backToStart = north.coordinate(at: distance, bearing: 180)
+ 
+        XCTAssertEqual(backToStart.latitude, start.latitude, accuracy: 0.001)
+        XCTAssertEqual(backToStart.longitude, start.longitude, accuracy: 0.001)
+    }
+
+    // MARK: - Random Coordinate Tests
+
+    func testRandomCoordinate_StaysWithinRadius() {
+        let center = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+        let radius: CLLocationDistance = 1000
+
+        for _ in 0..<10 {
+            let random = CLLocationCoordinate2D.random(center: center, radius: radius)
+            let distance = center.distance(to: random)
+
+            XCTAssertLessThanOrEqual(distance, radius, "Random coordinate should be within radius")
+            XCTAssertGreaterThanOrEqual(distance, 0)
         }
     }
 
+    // MARK: - Validation Tests
+
+    func testIsValid_ValidCoordinate_ReturnsTrue() {
+        let valid = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+
+        XCTAssertTrue(valid.isValid)
+    }
+
+    func testIsValid_InvalidLatitude_ReturnsFalse() {
+ 
+        let invalidLat = CLLocationCoordinate2D(latitude: 91.0, longitude: -3.1791)
+
+        XCTAssertFalse(invalidLat.isValid)
+    }
+
+    func testIsValid_InvalidLongitude_ReturnsFalse() {
+  
+        let invalidLon = CLLocationCoordinate2D(latitude: 51.4816, longitude: 181.0)
+
+        XCTAssertFalse(invalidLon.isValid)
+    }
+
+    // MARK: - Equality Tests
+
+    func testIsEqual_SameCoordinates_ReturnsTrue() {
+        
+        let coord1 = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+        let coord2 = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+
+        XCTAssertTrue(coord1.isEqual(to: coord2))
+    }
+
+    func testIsEqual_DifferentCoordinates_ReturnsFalse() {
+        
+        let coord1 = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+        let coord2 = CLLocationCoordinate2D(latitude: 51.4817, longitude: -3.1791)
+
+        XCTAssertFalse(coord1.isEqual(to: coord2))
+    }
 }
--- VeezuRideDemoTests/FareCalculationServiceTests.swift
+++ VeezuRideDemoTests/FareCalculationServiceTests.swift
@@ -1,16 +1,114 @@
-//
-//  FareCalculationServiceTests.swift
-//  VeezuRideDemoTests
-//
-//  Created by Ayodeji Ayankola on 29/10/2025.
-//
+import XCTest
+import CoreLocation
+@testable import VeezuRideDemo
 
-import Testing
+final class FareCalculationServiceTests: XCTestCase {
+    var sut: FareCalculationService!
 
-struct FareCalculationServiceTests {
-
-    @Test func <#test function name#>() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    override func setUp() {
+        super.setUp()
+        sut = FareCalculationService()
     }
 
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
+    // MARK: - Distance Calculation Tests
+
+    func testCalculateDistance_SameLocation_ReturnsZero() {
+        let location = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+
+        let distance = sut.calculateDistance(from: location, to: location)
+
+        XCTAssertEqual(distance, 0, accuracy: 1.0, "Distance between same location should be zero")
+    }
+
+    func testCalculateDistance_DifferentLocations_ReturnsPositiveDistance() {
+        let pickup = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+        let dropoff = CLLocationCoordinate2D(latitude: 51.4900, longitude: -3.1700)
+
+        let distance = sut.calculateDistance(from: pickup, to: dropoff)
+
+        XCTAssertGreaterThan(distance, 0, "Distance should be positive")
+        XCTAssertLessThan(distance, 2000, "Distance should be reasonable for Cardiff")
+    }
+
+    // MARK: - Fare Calculation Tests
+
+    func testCalculateFare_ShortDistance_ReturnsMinimumFare() {
+        
+        let pickup = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+        let dropoff = CLLocationCoordinate2D(latitude: 51.4817, longitude: -3.1791)
+
+        let fare = sut.calculateFare(from: pickup, to: dropoff, vehicleType: .sedan)
+
+        XCTAssertEqual(fare, AppConstants.Ride.minimumFare, "Short trips should have minimum fare")
+    }
+
+    func testCalculateFare_MediumDistance_CalculatesCorrectly() {
+        
+        let pickup = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+        let dropoff = CLLocationCoordinate2D(latitude: 51.5200, longitude: -3.1500)
+
+        let fare = sut.calculateFare(from: pickup, to: dropoff, vehicleType: .sedan)
+
+        XCTAssertGreaterThan(fare, AppConstants.Ride.minimumFare, "Medium distance should exceed minimum fare")
+        XCTAssertLessThan(fare, 20.0, "Fare should be reasonable for ~5km trip")
+    }
+
+    func testCalculateFare_DifferentVehicleTypes_AppliesCorrectMultiplier() {
+        
+        let pickup = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+        let dropoff = CLLocationCoordinate2D(latitude: 51.5200, longitude: -3.1500)
+
+        let sedanFare = sut.calculateFare(from: pickup, to: dropoff, vehicleType: .sedan)
+        let suvFare = sut.calculateFare(from: pickup, to: dropoff, vehicleType: .suv)
+        let luxuryFare = sut.calculateFare(from: pickup, to: dropoff, vehicleType: .luxury)
+
+        XCTAssertLessThan(sedanFare, suvFare, "SUV should be more expensive than sedan")
+        XCTAssertLessThan(suvFare, luxuryFare, "Luxury should be most expensive")
+    }
+
+    func testCalculateFare_ElectricVehicle_AppliesCorrectMultiplier() {
+        
+        let pickup = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+        let dropoff = CLLocationCoordinate2D(latitude: 51.5200, longitude: -3.1500)
+
+        let sedanFare = sut.calculateFare(from: pickup, to: dropoff, vehicleType: .sedan)
+        let electricFare = sut.calculateFare(from: pickup, to: dropoff, vehicleType: .electric)
+
+        XCTAssertGreaterThan(electricFare, sedanFare, "Electric should have slight premium")
+        XCTAssertLessThan(electricFare - sedanFare, 2.0, "Premium should be modest")
+    }
+
+    // MARK: - Rounding Tests
+
+    func testCalculateFare_RoundsToTwoDecimalPlaces() {
+      
+        let pickup = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+        let dropoff = CLLocationCoordinate2D(latitude: 51.4900, longitude: -3.1700)
+
+        let fare = sut.calculateFare(from: pickup, to: dropoff, vehicleType: .sedan)
+
+        let fareString = String(describing: fare)
+        let decimalParts = fareString.split(separator: ".")
+
+        if decimalParts.count > 1 {
+            XCTAssertLessThanOrEqual(decimalParts[1].count, 2, "Fare should have max 2 decimal places")
+        }
+    }
+
+    // MARK: - Performance Tests
+
+    func testCalculateFare_Performance() {
+   
+        let pickup = CLLocationCoordinate2D(latitude: 51.4816, longitude: -3.1791)
+        let dropoff = CLLocationCoordinate2D(latitude: 51.5200, longitude: -3.1500)
+
+        measure {
+            _ = sut.calculateFare(from: pickup, to: dropoff, vehicleType: .sedan)
+        }
+    }
 }
