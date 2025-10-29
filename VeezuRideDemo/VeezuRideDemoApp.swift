f17fb5f931fab9552bde2781019931c08ac4af38
Ayodeji Ayankola <your_verified_github_email@example.com>
2025-10-29 16:21:37 +0000

    set RideHailingAppView as root entry point in SwiftUI App

--- VeezuRideDemo/VeezuRideDemoApp.swift
+++ VeezuRideDemo/VeezuRideDemoApp.swift
@@ -1,17 +1,10 @@
-//
-//  VeezuRideDemoApp.swift
-//  VeezuRideDemo
-//
-//  Created by Ayodeji Ayankola on 29/10/2025.
-//
-
 import SwiftUI
 
 @main
 struct VeezuRideDemoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RideHailingAppView()
         }
     }
 }
